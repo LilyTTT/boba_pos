53806c3cc5621dee72d0866f520f1c6a3a00387d
Nishka Mittal <nishka06@tamu.edu>
2023-10-04 08:36:16 -0500

    queries update

--- queries/most_common_topping.sql
+++ queries/most_common_topping.sql
@@ -1,14 +1,5 @@
 -- most common topping
 
---SELECT toppings.*, COUNT(*) AS topping_count
---FROM toppings
---JOIN drinks on toppings.drink_id = drinks.drink_id
---JOIN orders on drinks.order_id = orders.order_id
---GROUP BY toppings.topping_id, toppings.name
---ORDER BY topping_count DESC
---LIMIT 1;
-
-
 SELECT
     toppings.name AS topping_name,
     COUNT(*) AS topping_count
