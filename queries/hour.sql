c167bbe27560d5fd62e82faa45fd3ae0e6b68d1f
Nishka Mittal <nishka06@tamu.edu>
2023-10-03 19:56:15 -0500

    added hour.sql

---
+++ queries/hour.sql
@@ -0,0 +1,9 @@
+--Realistic Sales History: select count of orders, sum of order total grouped by hour.
+
+SELECT
+    EXTRACT(HOUR FROM TO_TIMESTAMP(timestamp, 'YYYY-MM-DD HH24:MI:SS')) AS hour_of_day,
+    COUNT(*) AS order_count,
+    SUM(payment_amount) AS total_order_amount
+FROM orders
+GROUP BY hour_of_day
+ORDER BY hour_of_day;
