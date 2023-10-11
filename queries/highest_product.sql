1742a8ffbd8c454e76ec69eaf72391e0b18cbd96
Nishka Mittal <nishka06@tamu.edu>
2023-10-11 10:23:49 -0500

    edited highest_product.sql

--- queries/highest_product.sql
+++ queries/highest_product.sql
@@ -1,16 +1,6 @@
 --highest selling product
 
 -- made this query to find drink that is associated to the most orders
-SELECT
-    d.name AS highest_selling_drink,
-    COUNT(*) AS total_orders
-FROM
-    drinks d
-JOIN
-    orders o ON d.order_id = o.order_id
-GROUP BY
-    d.name
-ORDER BY
-    total_orders DESC
-LIMIT 1;
+SELECT TRIM(BOTH ' ' FROM d.name) AS highest_selling_drink, COUNT(*) AS total_orders FROM drinks d JOIN orders o ON d.order_id = o.order_id GROUP BY TRIM(BOTH ' ' FROM d.name) ORDER BY total_orders DESC LIMIT 1;
+
 
